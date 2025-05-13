8f8c0e8b9654ab8164b404702f2b2eed2c06bde3
Celine-Schroeder <193138175+Celine-Schroeder@users.noreply.github.com>
2025-05-14 01:43:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from leichtesprache.prompts import PROMPT_TEMPLATE, PROMPT_TEMPLATE_BASIC
 
 
 pinfo = {
-    #uebersetzt
+    # uebersetzt
     "Top k": "LLM-Parameter. Ein höherer Wert erzeugt einen abwechslungsreicheren Text",
     "Top p": "LLM-Parameter. Ein höherer Wert erzeugt einen abwechslungsreicheren Text",
     "Temp": "LLM-Parameter. Ein höhere Wert erhöht die Zufälligkeit der Antwort",
@@ -14,7 +14,9 @@ pinfo = {
 
 AVBL_LLMS = list_local_models()
 AVBL_LLM_CHOICES = sorted(list(set(p.LLM_CHOICES) & set(AVBL_LLMS)))
-DEFAULT_MODEL = p.MODEL if (p.MODEL in AVBL_LLMS) else (AVBL_LLM_CHOICES[0] if AVBL_LLM_CHOICES else None)
+DEFAULT_MODEL = (
+    p.MODEL if (p.MODEL in AVBL_LLMS) else (AVBL_LLM_CHOICES[0] if AVBL_LLM_CHOICES else None)
+)
 
 
 # =======================================================
@@ -25,10 +27,11 @@ with gr.Blocks(css="footer {visibility: hidden}") as demo:
 
     gr.Image(value="images/Logo-LeiSA-hellblau.png", show_label=False, height=80, elem_id="logo")
 
-# ======================================================================================================
-# Barrierefreiheit (analog Bsp. Bernau) - Kontrast (3 Modi) + Schriftgroessenaenderung + runde Buttons
-# ======================================================================================================
-    gr.HTML("""
+    # ======================================================================================================
+    # Barrierefreiheit (analog Bsp. Bernau) - Kontrast (3 Modi) + Schriftgroessenaenderung + runde Buttons
+    # ======================================================================================================
+    gr.HTML(
+        """
         <style>
             :root {
                 --font-scale: 1;
@@ -88,30 +91,42 @@ with gr.Blocks(css="footer {visibility: hidden}") as demo:
             <button class="accessibility-button" onclick="document.documentElement.style.setProperty('--font-scale', '1')">A</button>
             <button class="accessibility-button" onclick="document.documentElement.style.setProperty('--font-scale', '0.5')">A-</button>
         </div>
-    """)
+    """
+    )
 
-# ==============================================================
-# Hauptinterface
-# ==============================================================
+    # ==============================================================
+    # Hauptinterface
+    # ==============================================================
 
     simplify_text,
 
-    gr.Markdown("<h2 style='text-align: center;'>KI-Prototyp Leichte-Sprache-Assistent (LeiSA)</h2>")
-    gr.Markdown("Vereinfache Texte mit LLMs! - <a href='https://github.com/aihpi/leichte-sprache' target='_blank'>Zum Projekt auf GitHub</a>")
+    gr.Markdown(
+        "<h2 style='text-align: center;'>KI-Prototyp Leichte-Sprache-Assistent (LeiSA)</h2>"
+    )
+    gr.Markdown(
+        "Vereinfache Texte mit LLMs! - <a href='https://github.com/aihpi/leichte-sprache' target='_blank'>Zum Projekt auf GitHub</a>"
+    )
 
-# Beispiel rausgenommen
-    
-# Export-Button rausgenommen
-    
-    flagging_mode="never",  # Export Button deaktiviert
+    # Beispiel rausgenommen
 
-# Eingabetext u. Ausgabetext (in leichte Sprache)
+    # Export-Button rausgenommen
+
+    flagging_mode = ("never",)  # Export Button deaktiviert
+
+    # Eingabetext u. Ausgabetext (in leichte Sprache)
     with gr.Row():
-        input_text = gr.Textbox(label="Originaltext", lines = 17, autoscroll=True)
-        output_text = gr.Textbox(label="Leichte Sprache", lines=17, autoscroll=True, show_copy_button=True, elem_id ="output-box")
+        input_text = gr.Textbox(label="Originaltext", lines=17, autoscroll=True)
+        output_text = gr.Textbox(
+            label="Leichte Sprache",
+            lines=17,
+            autoscroll=True,
+            show_copy_button=True,
+            elem_id="output-box",
+        )
         copy_box = gr.Textbox(visible=False)
 
-    gr.HTML("""
+    gr.HTML(
+        """
             <style>
                 #output-box .svelte-1ipelgc {
                     transform: scale(1.3);
@@ -124,42 +139,61 @@ with gr.Blocks(css="footer {visibility: hidden}") as demo:
                 background-color: #ffe8cc;
                } 
             </style>
-        """)
+        """
+    )
 
     gr.Markdown("Hinweis zur Übersetzung: Dieser Text wurde mit einer KI erstellt.")
-    gr.Markdown("Bitte verarbeiten Sie keine personenbezogenen Daten mit dem Tool. Falls der Prototyp einmal nicht verfügbar sein sollte, kontaktieren Sie uns gern, wir kümmern uns darum.")
+    gr.Markdown(
+        "Bitte verarbeiten Sie keine personenbezogenen Daten mit dem Tool. Falls der Prototyp einmal nicht verfügbar sein sollte, kontaktieren Sie uns gern, wir kümmern uns darum."
+    )
 
-            
-# ausklappbare Einstellungen ausgeblendet vom Frontend
+    # ausklappbare Einstellungen ausgeblendet vom Frontend
     with gr.Accordion(label="Einstellungen", open=False, visible=False):
-        model = gr.Dropdown(choices=AVBL_LLM_CHOICES, value=DEFAULT_MODEL, label="KI-Modell auswählen", allow_custom_value=True)
-        use_rules = gr.Checkbox(value=p.USE_RULES, label="Advanced Prompt (verwende Regeln)", info="Verwende die Regeln zur Vereiinfachung", visible=False)
+        model = gr.Dropdown(
+            choices=AVBL_LLM_CHOICES,
+            value=DEFAULT_MODEL,
+            label="KI-Modell auswählen",
+            allow_custom_value=True,
+        )
+        use_rules = gr.Checkbox(
+            value=p.USE_RULES,
+            label="Advanced Prompt (verwende Regeln)",
+            info="Verwende die Regeln zur Vereiinfachung",
+            visible=False,
+        )
         top_k = gr.Slider(1, 10, value=p.TOP_K, step=1, label="Top k", info=pinfo.get("Top k"))
         top_p = gr.Slider(0.1, 1, value=p.TOP_P, step=0.1, label="Top p", info=pinfo.get("Top p"))
-        temp = gr.Slider(0.1, 10, value=p.TEMP, step=0.1, label ="Temp", info=pinfo.get("Temp"))
+        temp = gr.Slider(0.1, 10, value=p.TEMP, step=0.1, label="Temp", info=pinfo.get("Temp"))
 
-# Buttons: Vereinfachen, Kopieren und Loeschen
+    # Buttons: Vereinfachen, Kopieren und Loeschen
     with gr.Row():
         vereinfachen_btn = gr.Button("Vereinfachen!", variant="primary")
-        vereinfachen_btn.click(fn=simplify_text, inputs=[input_text, model, use_rules, top_k, temp], outputs=[output_text])
+        vereinfachen_btn.click(
+            fn=simplify_text,
+            inputs=[input_text, model, use_rules, top_k, temp],
+            outputs=[output_text],
+        )
         clear_button = gr.Button("Löschen", variant="secondary")
-        #clear_button.click(fn=lambda: "", outputs=input_text)   #leert nur Eingabe-Textbox
-        clear_button.click(fn=lambda: ("", ""), outputs=[input_text, output_text])  # leert beide Textfelder
+        # clear_button.click(fn=lambda: "", outputs=input_text)   #leert nur Eingabe-Textbox
+        clear_button.click(
+            fn=lambda: ("", ""), outputs=[input_text, output_text]
+        )  # leert beide Textfelder
 
-#Hinweis: Parameter bleiben unveraendert bei dieser Definition des clear-buttons
-    
-                
-# ==============================================================
-# Footer
-# ==============================================================
-        
-    gr.HTML("""
+    # Hinweis: Parameter bleiben unveraendert bei dieser Definition des clear-buttons
+
+    # ==============================================================
+    # Footer
+    # ==============================================================
+
+    gr.HTML(
+        """
         <div style="text-align: center; margin-top: 40px;">
             <p style="font-size: 12px; color: #666;">Der KI-Prototyp LeiSA ist ein gemeinsames Projekt der DigitalAgentur Brandenburg GmbH<sup>1</sup> und des KI-Servicezentrums (KISZ) vom Hasso-Plattner-Institut<sup>2</sup></p>
             <p style="font-size: 10px; color: #999;"><sup>1</sup>Gefördert durch das Ministerium der Justiz und für Digitalisierung des Landes Brandenburg.</p>
             <p style="font-size: 10px; color: #999;"><sup>2</sup>Gefördert vom Bundesministerium für Bildung und Forschung.</p>
         </div>
-    """)
+    """
+    )
 
 # Start
 if __name__ == "__main__":
